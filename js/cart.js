60fcaa5ef28d553554fc7b5538c9ff05c300af84
Mahin Hasan <mahinmtrs@gmail.com>
2023-10-04 05:18:27 +0600

    showed discount calculations

--- js/cart.js
+++ js/cart.js
@@ -7,24 +7,10 @@ function getFirstCardDetails() {
     // for cart calculation
     const totalPriceInitial = getItemNumber('total-price');
     const totalPrice = itemPrice + totalPriceInitial;
-    // purchase btn validation
-    const purchaseButton = document.getElementById('make-purchase-btn');
-    if (totalPrice > 0) {
-        purchaseButton.removeAttribute('disabled');
-    }
-    else {
-        purchaseButton.setAttribute('disabled', true);
-    }
-    // coupon btn
-    const deleteButton = document.getElementById('coupon-apply-btn');
 
-    if (totalPrice > 200) {
-        deleteButton.removeAttribute('disabled');
-    }
-    else {
-        deleteButton.setAttribute('disabled', true);
-    }
     setItemNumber('total-price', totalPrice);
+    // 
+    btnValidation();
 }
 
 
@@ -38,15 +24,20 @@ function getSecondCardDetails() {
     const totalPriceInitial = getItemNumber('total-price');
     const totalPrice = itemPrice + totalPriceInitial;
     setItemNumber('total-price', totalPrice);
+    btnValidation();
+
 }
 // trying coupon discount btn
-document.getElementById('coupon-apply-btn').addEventListener('click',function(){
+document.getElementById('coupon-apply-btn').addEventListener('click', function () {
     const typedCoupon = document.getElementById('coupon-field').value;
-    if(typedCoupon==='SELL200'){
+    if (typedCoupon === 'SELL200') {
         const totalPriceInitial = getItemNumber('total-price');
-        const discount =  (0.2 * totalPriceInitial).toFixed(2);
-        setItemNumber('discount',discount);
-        console.log(totalPriceInitial,discount);
+        const discount = (0.2 * totalPriceInitial).toFixed(2);
+        setItemNumber('discount', discount);
+        console.log(totalPriceInitial, discount);
+        const totalWithDiscount = totalPriceInitial - discount;
+        console.log(totalWithDiscount);
+        setItemNumber('net-total',totalWithDiscount);
     }
 })
 
@@ -77,6 +68,25 @@ function displayClickedItemName(itemName) {
     parentDiv.appendChild(h3);
 }
 
+function btnValidation() {
+    // purchase btn validation
+    const totalPriceCheck = getItemNumber('total-price');
+    const purchaseButton = document.getElementById('make-purchase-btn');
+    if (totalPriceCheck > 0) {
+        purchaseButton.removeAttribute('disabled');
+    }
+    else {
+        purchaseButton.setAttribute('disabled', true);
+    }
+    // coupon btn validation
+    const deleteButton = document.getElementById('coupon-apply-btn');
+    if (totalPriceCheck > 200) {
+        deleteButton.removeAttribute('disabled');
+    }
+    else {
+        deleteButton.setAttribute('disabled', true);
+    }
+}
 
 /*
 document.getElementById('coupon-apply-btn').addEventListener('click',function(){
